d4956d3c4d67c8443fae00b22905909401a69bac
Larissa <lala.objetivo@gmail.com>
2025-12-08 15:15:44 -0300

    TEST

---
+++ algorithms/__init__.py
@@ -0,0 +1,11 @@
+from .bubble import bubble_sort
+from .insertion import insertion_sort
+from .merge import merge_sort
+from .quick import quick_sort
+
+__all__ = [
+    "bubble_sort",
+    "insertion_sort",
+    "merge_sort",
+    "quick_sort",
+]
